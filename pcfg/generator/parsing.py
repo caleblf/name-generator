7d7646d7290bcc10434fc696cdd2f58b1bf5a3db
Iguanotron <clucasfoley@gmail.com>
2018-01-14 12:21:06 -0500

    Check types of yaml fields

--- pcfg/generator/parsing.py
+++ pcfg/generator/parsing.py
@@ -14,14 +14,25 @@ def unzip(l):
 def make_form_placeholder(v):
     if isinstance(v, list):
         return language.ProbabilisticForm([], [])
+    if not isinstance(v, str):
+        raise ValueError('Non-string form expression in tag')
     if v.startswith('('): # concatenation
         if not v.endswith(')'):
-            raise ValueError('Unmatched parenthesis in form expression: %s' % v)
+            raise ValueError('Unmatched parenthesis in tagged form expression: %s' % v)
         return language.ConcatenativeForm([])
     if v.startswith('$'): # tag
-        raise ValueError('Tag at top level in form expression: %s' % v)
+        raise ValueError('Tag at top level in tagged form expression: %s' % v)
     # literal
     return language.LiteralForm(v)
+
+
+def parse_probabilistic_form_clause(s):
+    elements = s.split(maxsplit=1)
+    try:
+        odds = int(elements[0])
+        return parse_form_exp(elements[1]), odds
+    except ValueError:
+        return parse_form_exp(s.strip()), 1
 
 
 def load_from_file(filename):
@@ -32,9 +43,16 @@ def load_from_file(filename):
     except yaml.error.YAMLError as e:
         raise ValueError('Improperly formatted YAML file') from e
     name = data['name'];
-    tagged_forms = {k: make_form_placeholder(v) for k, v in data['forms'].items()}
+    if not isinstance(name, str):
+        raise ValueError('Non-string language name')
+    forms = data.get('forms', {})
+    if not isinstance(forms, dict):
+        raise ValueError('Tagged forms table not a dictionary')
+    tagged_forms = {k: make_form_placeholder(v) for k, v in forms.items()}
 
     def parse_form_exp(exp):
+        if not isinstance(v, str):
+            raise ValueError('Non-string form expression')
         if exp.startswith('('): # concatenation
             if not exp.endswith(')'):
                 raise ValueError('Unmatched parenthesis in form expression: %s' % exp)
@@ -46,17 +64,9 @@ def load_from_file(filename):
             try:
                 return tagged_forms[exp[1:]]
             except KeyError:
-                raise ValueError('Unknown key in form expression: %s' % exp)
+                raise ValueError('Unknown tag in form expression: %s' % exp)
         # literal
         return language.LiteralForm(exp)
-
-    def parse_probabilistic_form_clause(s):
-        elements = s.split(maxsplit=1)
-        try:
-            odds = int(elements[0])
-            return parse_form_exp(elements[1]), odds
-        except ValueError:
-            return parse_form_exp(s.strip()), 1
 
     # parse root form
     root_form = parse_form_exp(data['root'])
@@ -64,10 +74,8 @@ def load_from_file(filename):
     # parse tagged forms
     for tag, form in tagged_forms.items():
         if isinstance(form, language.ProbabilisticForm):
-            form.set_forms(*unzip(map(
-                parse_probabilistic_form_clause,
-                data['forms'][tag])))
+            form.set_forms(*unzip(map(parse_probabilistic_form_clause, forms[tag])))
         elif isinstance(form, language.ConcatenativeForm):
-            form.set_forms(parse_form_exp(data['forms'][tag]).forms)
+            form.set_forms(parse_form_exp(forms[tag]).forms)
 
     return language.Language(name, root_form)
