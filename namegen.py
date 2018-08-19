86be4f223e77c8d5aeb90353dc493aedbe56b32a
Iguanotron <clucasfoley@gmail.com>
2018-08-19 00:09:44 -0400

    Try to detect color support automatically

--- namegen.py
+++ namegen.py
@@ -12,33 +12,46 @@ except ImportError:
     print('Missing third party dependency: `pyyaml`', file=sys.stderr)
     sys.exit(1)
 
+import os
 import argparse
 
 from generator import interactive
 
 
+def device_supports_color():
+    """Return whether this device supports ANSI color codes.
+    Adapted from django.core.management.color.supports_color
+    """
+    plat = sys.platform
+    supported_platform = plat != 'Pocket PC' and (plat != 'win32' or
+                                                  'ANSICON' in os.environ)
+    is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
+    return supported_platform and is_a_tty
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('commands',
+    parser.add_argument('commandfile',
                         type=str,
                         nargs='?',
-                        help='Commands to execute')
+                        help='file of commands to execute')
     parser.add_argument('-i', '--interactive',
                         action='store_true',
-                        help='If commands are passed, enter interactive mode '
-                             'after executing commands')
+                        help='if commandfile is passed, enter interactive '
+                             'mode after executing commands')
     parser.add_argument('-c', '--color',
                         action='store_true',
-                        help='Use colored text for names')
+                        help='use colored text for names (requires '
+                             '`colorama` package on Windows)')
     parser.add_argument('-w', '--suppress-warnings',
                         action='store_true',
-                        help='Suppress compatibility warnings')
+                        help='suppress compatibility warnings')
     parser.add_argument('-q', '--quiet',
                         action='store_true',
-                        help='Suppress success confirmation messages')
+                        help='suppress success confirmation messages')
     args = parser.parse_args()
 
-    if args.color:
+    if args.color and not device_supports_color():
         try:
             import colorama  # for MS Windows color support
             colorama.init()
